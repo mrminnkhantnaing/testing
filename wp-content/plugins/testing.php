18c625dc3ee6f278bd5ace39cca00879d3902afc
mrminnkhantnaing <mr.minnkhantnaing@gmail.com>
2022-02-20 12:55:05 +0400

    plugin

---
+++ wp-content/plugins/testing.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Testing
+ *
+ * Plugin Name: Testing
+ * Plugin URI:  https://wordpress.org/plugins/testing/
+ * Description: Enables the WordPress Testing and the old-style Edit Post screen with TinyMCE, Meta Boxes, etc. Supports the older plugins that extend this screen.
+ * Version:     1.6.2
+ * Author:      Frank
+ * Author URI:  https://frank.com
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: testing
+ * Domain Path: /languages
+ * Requires at least: 4.9
+ * Tested up to: 5.8
+ * Requires PHP: 5.2.4
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
